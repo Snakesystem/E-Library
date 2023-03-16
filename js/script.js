98b65d76f4154d332e5e32b3d0b0b51a27619097
feryirawan <ir15y4hh@gmail.com>
2023-03-16 11:43:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Memanggil doom
+document.addEventListener("DOMContentLoaded", () => {
+
+    const submitBook/* HTMLFormElement */ = document.getElementById("inputBook");
+    const searchBook = document.getElementById("searchBook");
+
+    submitBook.addEventListener("submit", (event) => {
+        event.preventDefault();
+        addBook();
+    });
+
+    searchBook.addEventListener("submit", (event) => {
+        event.preventDefault();
+        searchTheBook();
+    });
+
+    if(localStorageExist()) {
+        loadBookFromStorage ();
+    };
+
+});
+
+document.addEventListener("ondatasaved", () => {
+    console.log("Data berhasil disimpan.")
+});
+document.addEventListener("ondataloaded", () => {
+   refreshBookData();
+});
---
+++ js/search.js
@@ -0,0 +1,19 @@
+// Fungsi untuk melakukan pencarian buku
+
+const searchTheBook =  () => {
+    let search = document.getElementById('searchBookTitle').value;
+
+    let bookCard = document.getElementsByClassName('book-item');
+
+    for (bookItem of bookCard) {
+        let bookTitle = bookItem.innerText.toUpperCase();
+        let searchBook = bookTitle.search(search.toUpperCase());
+
+        if(searchBook != -1) {
+            bookItem.style.display = ""
+        } else { 
+            bookItem.style.display = "none";
+            console.log(searchBook);
+        };
+    } 
+};
